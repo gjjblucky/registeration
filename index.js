12c4ec232a616372f34003eb44e331852342efd4
gjjblucky <pahadion007@gmail.com>
2022-07-23 17:13:17 +0530

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/user_management_system");
+
+const express = require("express");
+const app = express();
+
+//for user routes
+const userRoute = require('./routes/userRoute');
+app.use('/',userRoute);
+
+
+
+app.listen(3000,function(){
+    console.log("Server is runnnig...");
+});
